daa76542a9fe48fd2793413639284a458a89121c
Vander Vieira <vandervsvieira@gmail.com>
2023-05-05 04:20:24 -0300

    show map with current loc marker

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState , useRef} from 'react';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { View } from 'react-native';
+import {
+    requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,
+    LocationObject,
+    watchPositionAsync,
+    LocationAccuracy
+} from 'expo-location';
+
+import { defaultScreen } from './styles/general';
+import { mapStyle, customMap } from './styles/map';
 
 export default function App() {
+    const [location, setLocation] = useState<LocationObject | null>(null);
+
+    const mapRef = useRef<MapView>(null); // Referencia usada para pinar o marcador no mapa 
+
+    async function requestLocationPermission(){ // Permissões do aparelho para rastrear localização
+        const { granted } = await requestForegroundPermissionsAsync();
+        
+        if(granted){
+            const currentPosition = await getCurrentPositionAsync();
+            setLocation(currentPosition);
+        }
+    }
+
+  useEffect(() => { // Pede permissão para o usuário 
+    requestLocationPermission();
+  },[]);
+
+  useEffect(() => { // Atualiza a localização do usuário em tempo real
+    watchPositionAsync({
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000, //De quanto em quanto tempo atualiza a localização
+        distanceInterval: 1
+    }, (response) => {
+        setLocation(response); //Funcão que atualiza a localização
+        mapRef.current?.animateCamera({ // Atualiza o mapa para centralizar no marcador
+            //pitch: 70,
+            center: response.coords
+        })
+    });
+  },[]);
+    
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={defaultScreen.container}>
+
+        {
+        location &&    
+        <MapView 
+            ref={mapRef}
+            style={mapStyle.map}
+            provider={PROVIDER_GOOGLE}
+            customMapStyle={customMap}
+            initialRegion={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005
+            }}
+        >
+            <Marker 
+            coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude
+            }}
+            />
+        </MapView>
+        }
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/general.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const defaultScreen = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+        width: '100%'
+    }
+});
+  
